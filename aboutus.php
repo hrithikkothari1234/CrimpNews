708fa2f3292a4559b6f1d78233f139c01b096da1
hritikkothari <kotharihrithik1@gmail.com>
2019-12-22 17:30:50 +0530

    Changed about us col-lg

--- aboutus.php
+++ aboutus.php
@@ -2,7 +2,7 @@
 
 <section class="container">
     <!-- Subscribe page content holder -->
-    <div class="page-content col-lg-8 col-md-6" id="content">
+    <div class="page-content col-lg-10 col-md-8" id="content">
         <div class="card p-4">
             <div class="card-body">
                 <h3 class="text-center" style="font-family: Playfair Display, serif;">
