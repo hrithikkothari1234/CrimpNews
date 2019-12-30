99dc0d14e6e81aa17c876eec60ca1229215ecca4
hritikkothari <kotharihrithik1@gmail.com>
2019-12-30 21:49:13 +0530

    Added footer with content and scroll-to-top button

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,35 @@
+        <footer>
+            <div class="row">
+                <div class="offset-lg-2 col-lg-1">
+                    <img src="public/images/icon.jpg" onclick="document.documentElement.scrollTop = 0;">
+                </div>
+                <h7 class="offset-lg-1 col-lg-4">
+                    Crimp News is an online platform to help management students get the important day to day business news inorder to
+                    ease their task of reading industry reports , podcast and other articles which will improve their performance during
+                    Group Discussion and in Interviews.
+                </h7>
+            </div>
+        </footer>
+
+        <div class="lower-footer">
+            <div class="row">
+                <div class="offset-lg-2 col-lg-2">
+                    <span id="year">
+                    </span>
+                    <h7 onclick="document.documentElement.scrollTop = 0;">
+                        | Crimp News
+                    </h7>
+                </div>
+                <div class="offset-lg-7 col-lg-1">
+                    <h5 class="scroll-top-btn" onclick="document.documentElement.scrollTop = 0;">
+                        <i class="fa fa-angle-up fa-fw"></i>
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <!-- footer ends-->
+
+        <!-- script cdns -->
         <script
         src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         crossorigin="anonymous"></script>
@@ -10,12 +42,11 @@
 
         <script type="text/javascript" src="public/js/index.js"></script>
 
-
         <script>
             // Get the current year for the copyright
             $('#year').text(new Date().getFullYear());
         </script>
 
-</body>
+    </body>
 
 </html>
