2cba4252ce97efcbcac0cfd56bf54fd005d8abb7
hritikkothari <kotharihrithik1@gmail.com>
2019-12-30 22:43:50 +0530

    Made footer for new theme responsive

--- include/footer.php
+++ include/footer.php
@@ -1,30 +1,28 @@
         <footer>
-            <div class="row">
-                <div class="offset-lg-2 col-lg-1">
-                    <img src="public/images/icon.jpg" onclick="document.documentElement.scrollTop = 0;">
-                </div>
-                <h7 class="offset-lg-1 col-lg-4">
+            <div class="pull-left">
+                <img src="public/images/icon.jpg" onclick="document.documentElement.scrollTop = 0;">
+            </div>
+            <span>
+                <h7>
                     Crimp News is an online platform to help management students get the important day to day business news inorder to
                     ease their task of reading industry reports , podcast and other articles which will improve their performance during
                     Group Discussion and in Interviews.
                 </h7>
-            </div>
+            </span>
         </footer>
 
         <div class="lower-footer">
-            <div class="row">
-                <div class="offset-lg-2 col-lg-2">
-                    <span id="year">
-                    </span>
-                    <h7 onclick="document.documentElement.scrollTop = 0;">
-                        | Crimp News
-                    </h7>
-                </div>
-                <div class="offset-lg-7 col-lg-1">
-                    <h5 class="scroll-top-btn" onclick="document.documentElement.scrollTop = 0;">
-                        <i class="fa fa-angle-up fa-fw"></i>
-                    </h5>
-                </div>
+            <div class="pull-left">
+                <span id="year">
+                </span>
+                <h7 onclick="document.documentElement.scrollTop = 0;">
+                    | Crimp News
+                </h7>
+            </div>
+            <div class="pull-right">
+                <h5 class="scroll-top-btn" onclick="document.documentElement.scrollTop = 0;">
+                    <i class="fa fa-angle-up fa-fw"></i>
+                </h5>
             </div>
         </div>
         <!-- footer ends-->
