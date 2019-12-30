5f3e277320f71625bb8542e2616ba0db0fdc0c5b
hritikkothari <kotharihrithik1@gmail.com>
2019-12-30 18:27:08 +0530

    Updated about us page with the new theme

--- aboutus.php
+++ aboutus.php
@@ -1,19 +1,26 @@
 <?php require_once('include/header.php'); ?>
 
-<section class="container">
-    <!-- Subscribe page content holder -->
-    <div class="page-content col-lg-10" id="content">
-        <div class="card p-4">
-            <div class="card-body">
-                <h3 class="text-center" style="font-family: Playfair Display, serif;">
-                    About Us
-                </h3>
-                <hr>
-                <h6 class='text-muted'>
-                    Crimp News is an online platform to help management students get the important day to day business news inorder to
-                    ease their task of reading industry reports , podcast and other articles which will improve their performance during
-                    Group Discussion and in Interviews.
-                </h6>
+<div class="container">
+    <div class="row">
+    <!-- About us page content holder -->
+        <div class="col-lg-8 subscribe-page-content">
+            <div class="card p-4">
+                <div class="card-body">
+                    <h3 class="text-center" style="font-family: Playfair Display, serif;">
+                        About
+                            <span style="color: rgb(255, 61, 46);">
+                                Us
+                            <span>
+                        <i class="fa fa-handshake-o fa-fw"></i>
+                    </h3>
+                    <hr>
+                    <img src="public/images/icon.jpg">
+                    <h5 class='text-muted'>
+                        Crimp News is an online platform to help management students get the important day to day business news inorder to
+                        ease their task of reading industry reports , podcast and other articles which will improve their performance during
+                        Group Discussion and in Interviews.
+                    </h5>
+                </div>
             </div>
         </div>
     </div>
