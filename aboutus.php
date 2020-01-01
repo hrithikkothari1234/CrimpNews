497c9f7583bbf6797ffb92ed4c3848004b1e09e3
hritikkothari <kotharihrithik1@gmail.com>
2020-01-02 00:31:54 +0530

    Removed footer from about us page

--- aboutus.php
+++ aboutus.php
@@ -1,29 +1,49 @@
 <?php require_once('include/header.php'); ?>
 
-<div class="container">
-    <div class="row">
-    <!-- About us page content holder -->
-        <div class="col-lg-8 subscribe-page-content">
-            <div class="card p-4">
-                <div class="card-body">
-                    <h3 class="text-center" style="font-family: Playfair Display, serif;">
-                        About
-                            <span style="color: rgb(255, 61, 46);">
-                                Us
-                            <span>
-                        <i class="fa fa-handshake-o fa-fw"></i>
-                    </h3>
-                    <hr>
-                    <img src="public/images/icon.jpg">
-                    <h5 class='text-muted'>
-                        Crimp News is an online platform to help management students get the important day to day business news inorder to
-                        ease their task of reading industry reports , podcast and other articles which will improve their performance during
-                        Group Discussion and in Interviews.
-                    </h5>
+    <div class="container">
+        <div class="row">
+        <!-- About us page content holder -->
+            <div class="col-lg-8 subscribe-page-content">
+                <div class="card p-4">
+                    <div class="card-body">
+                        <h3 class="text-center" style="font-family: Playfair Display, serif;">
+                            About
+                                <span style="color: rgb(255, 61, 46);">
+                                    Us
+                                <span>
+                            <i class="fa fa-handshake-o fa-fw"></i>
+                        </h3>
+                        <hr>
+                        <img src="public/images/icon.jpg">
+                        <h5 class='text-muted'>
+                            Crimp News is an online platform to help management students get the important day to day business news inorder to
+                            ease their task of reading industry reports , podcast and other articles which will improve their performance during
+                            Group Discussion and in Interviews.
+                        </h5>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<?php require_once("include/footer.php"); ?>
+    <!-- script cdns -->
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"
+    crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="public/js/index.js"></script>
+
+    <script>
+        // Get the current year for the copyright
+        $('#year').text(new Date().getFullYear());
+    </script>
+
+</body>
+
+</html>
